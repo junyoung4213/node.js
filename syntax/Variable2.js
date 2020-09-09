71423249bdc64bd5a677b2efa1ce9c44a7e86bb2
junyoung4213 <junyoung4213@gmail.com>
2020-09-09 17:04:57 +0900

    5

---
+++ syntax/Variable2.js
@@ -0,0 +1,9 @@
+var name = "junyoung";
+var letter =
+  "Dear " +
+  name +
+  " Lorem ipsum dolor sit amet, " +
+  name +
+  " consectetur adipisicing elit, sed do eiusmod " +
+  name;
+console.log(letter);
